943feaa36a2ed4f9f6b509dbb008dcd455eff0ca
frostyx-vk <vk.xam@yandex.ru>
2024-01-16 11:00:52 +0700

    fixation

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,10 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="wrapper" id="app">
+    <div class="content-wrapper">
+      <div class="container">
+        Baref
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,12 +20,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
